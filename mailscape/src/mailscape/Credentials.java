e93a3fb449e72e19b26ac27db5371db9da023301
LkM14042002 <lkmcourses123@gmail.com>
2021-10-22 13:09:07 +0530

    SENDING MAIL AND RECEIVING MAIL TASK COMPLETED

---
+++ mailscape/src/mailscape/Credentials.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mailscape;
+
+/**
+ *
+ * @author laxmikanth
+ */
+public class Credentials {
+    final static String username="meeth269@gmail.com";
+    final static String password="hneoioitlbqyxpsp";
+    final static String DB_URL="";
+    final static String USER="LaxmiKanth";
+    final static String pass="K@nt#43280";
+}
